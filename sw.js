62e9d0909845240c1624927b94e82338a47c4847
nikhil <nikhilatoliya12@gmail.com>
2021-05-22 12:18:44 +0530

    pwa uff

--- sw.js
+++ sw.js
@@ -4,7 +4,22 @@ var files = [
 
     './',
     './index.html',
+    "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i",
+    "assets/vendor/aos/aos.css",
+    "assets/vendor/bootstrap/css/bootstrap.min.css",
+    "assets/vendor/bootstrap-icons/bootstrap-icons.css",
+    "assets/vendor/boxicons/css/boxicons.min.css",
+    "assets/vendor/glightbox/css/glightbox.min.css",
+    "assets/vendor/remixicon/remixicon.css",
+    "assets/vendor/swiper/swiper-bundle.min.css",
     './assets/css/style.css',
+    "assets/vendor/aos/aos.js",
+    "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+    "assets/vendor/glightbox/js/glightbox.min.js",
+    "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+    "assets/vendor/php-email-form/validate.js",
+    "assets/vendor/purecounter/purecounter.js",
+    "assets/vendor/swiper/swiper-bundle.min.js",
     './images/logo192.jpg',
     './img/product-details/add-12.png',
     './img/product-details/product-detail-1.jpg',
@@ -31,11 +46,6 @@ var files = [
     '/assets/js/main.js',
     '/assets/js/success.js',
     '/assets/vendor',
-
-    
-
-
-
 
 ]
 
