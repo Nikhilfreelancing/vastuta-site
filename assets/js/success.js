4c8015feaf6790a7265f8fc645262bfc843bf624
nikhil <nikhilatoliya12@gmail.com>
2021-05-23 14:59:45 +0530

    cont

--- assets/js/success.js
+++ assets/js/success.js
@@ -1,9 +1,4 @@
 function success(){
 	
-     //     document.getElementById('name').value='';
-     //   document.getElementById('phone').value='';
-    	// document.getElementById('email').value='';
-        
-     //    document.getElementById('message').value='';
      alert("Thanks for submiiting your query!!")
 }
